0907bd498c2a4f4d99b9c0deef13698faeab8ef3
lindsy12 <lindsy339@gmail.com>
2025-09-09 11:52:45 +0100

    login message has been updated

--- Aunthentication/Login.php
+++ Aunthentication/Login.php
@@ -32,7 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
         exit;
     } else {
-        $error = "Invalid email or password!";
+        $error = "Invalid credentials!";
     }
 }
 ?>
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login - GlowSkin</title>
+    <title>Login - Skincare</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
         * {
